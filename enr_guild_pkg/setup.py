233de58d5d0d664a567ea81298cc847e6173fafc
Nithin K <nithinkarthikeyan01@gmail.com>
2025-07-04 18:19:26 +0530

    Revert "modify setup.py; modify setup.cfg; add bottle_rf.py"
    This reverts commit aef08ae581b61163f7fcfa2b3d27107d4d4548b5.

--- enr_guild_pkg/setup.py
+++ enr_guild_pkg/setup.py
@@ -25,8 +25,7 @@ setup(
         'talker = enr_guild_pkg.talker:main',
         'relay = enr_guild_pkg.relay:main',
         'circle = enr_guild_pkg.circle:main',
-        'webcam = enr_guild_pkg.webcam:main',
-        'bottle_rf = enr_guild_pkg.bottle_rf:main'
+        'webcam = enr_guild_pkg.webcam:main'
         ],
     },
 )
